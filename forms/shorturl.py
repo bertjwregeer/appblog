16322f1858928059a3c6b81a2b2603e61210def6
Bert JW Regeer <bertjw@regeer.org>
2010-02-09 03:24:31 +0000

    Move the form from a specific file to the model itself. There is no reason as to why the form should not be in the same file as the model. The two need to be maintained together, especially since there is a lot of validation that needs to be done on the form to make sure that input is valid.
    TODO: See if we can move the validation to the actual model itself thereby making it harder to make a mistake while programming using the model.
    
    
    
    
    git-svn-id: http://svn.0x58.com/repos/BertJWRegeer/Projects/appblog/trunk@126 3d59c550-c999-dd11-821d-0030482bb012

--- forms/shorturl.py
+++ forms/shorturl.py
@@ -1,5 +1,5 @@
 ###
- # Copyright (c) 2009 Bert JW Regeer;
+ # Copyright (c) 2010 Bert JW Regeer;
  #
  # Permission to use, copy, modify, and distribute this software for any
  # purpose with or without fee is hereby granted, provided that the above
@@ -15,24 +15,4 @@
  #
 ###
 
-
-from google.appengine.ext.webapp import template
-from google.appengine.ext.db import djangoforms
-
-# Django imports
-from django import newforms as forms
-
-import models
-
-REDIRECT_CHOICES = (
-	(301, "301 - Permanent Redirect"),
-	(302, "302 - Temporary Redirect"),
-	(403, "403 - Access Denied"),
-	(404, "404 - File Not Found"),
-	(500, "500 - Internal Server Error")
-)
-	
-class ShorturlForm(djangoforms.ModelForm):
-	httpcode = forms.IntegerField(widget=forms.Select(choices=REDIRECT_CHOICES), required=True, label='HTTP code')
-	class Meta:
-		model = models.Shorturl 
+from models.shorturl import ShorturlForm
--- models/shorturl.py
+++ models/shorturl.py
@@ -1,5 +1,5 @@
 ###
- # Copyright (c) 2009 Bert JW Regeer;
+ # Copyright (c) 2010 Bert JW Regeer;
  #
  # Permission to use, copy, modify, and distribute this software for any
  # purpose with or without fee is hereby granted, provided that the above
@@ -15,17 +15,66 @@
  #
 ###
 
+import logging 
 
 from google.appengine.ext import db
+from google.appengine.ext.webapp import template
+from google.appengine.ext.db import djangoforms
+
+# Django imports
+from django import newforms as forms
+from django.newforms.util import ErrorList, ValidationError
 
 import utils
 
+REDIRECT_CHOICES = (
+	(301, "301 - Permanent Redirect"),
+	(302, "302 - Temporary Redirect"),
+	(403, "403 - Access Denied"),
+	(404, "404 - File Not Found"),
+	(500, "500 - Internal Server Error")
+)
+
 class Shorturl(db.Model):
 	uripath = db.StringProperty(verbose_name="Path", required=True)
-	httpcode = db.IntegerProperty(verbose_name="HTTP code", required=True, default=301, choices=[301, 302, 403, 404, 500])
+	httpcode = db.IntegerProperty(verbose_name="HTTP code", required=True, default=301, choices=[x[0] for x in REDIRECT_CHOICES])
 	location = db.StringProperty(verbose_name="Location")
 
 	@property
 	def hits(self):
 		count = utils.Count()
 		return count.curcount("shorturl", self.uripath)
+		
+	
+
+class ShorturlForm(djangoforms.ModelForm):
+	httpcode = forms.IntegerField(widget=forms.Select(choices=REDIRECT_CHOICES), required=True, label='HTTP code')
+
+	def clean_uripath(self):
+		cleaned_data = self.clean_data
+		uripath = cleaned_data.get('uripath')
+		
+		# We are going to test for uniqueness before we consider this field clean
+		if self.instance == None or self.instance.uripath != uripath:
+			if Shorturl.all().filter("uripath =", uripath).fetch(1):
+				raise forms.ValidationError("Path already exists.")
+		
+		
+		return uripath
+		
+	def clean_location(self):
+		cleaned_data = self.clean_data
+		httpcode = cleaned_data.get('httpcode')
+		location = cleaned_data.get('location')
+
+		if httpcode == 301 or httpcode == 302:
+			if len(location):
+				return location
+			else:
+				raise forms.ValidationError("Location required when using status code 301 or 302.")
+
+		return location
+
+	class Meta:
+		model = Shorturl
+	
