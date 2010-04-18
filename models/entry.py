ec011090a841ecbb0efbc7f5c57d9ba77b0201d3
Bert JW Regeer <bertjw@regeer.org>
2010-04-18 10:57:31 +0000

    There were a whole lot of changes, the major ones were:
    - slugs are not correctly computed
    - the HTML is correctly generated from the markdown that is entered, and cached correctly
    - delete function was moved around
    - Inherits from the new dbhook.py that was introduced in an earlier commit. This was done to make it easier to re-use the code, and because it looks neater to have hooks that are called before a put action
    
    
    git-svn-id: http://svn.0x58.com/repos/BertJWRegeer/Projects/appblog/trunk@168 3d59c550-c999-dd11-821d-0030482bb012

--- models/entry.py
+++ models/entry.py
@@ -15,7 +15,8 @@
  #
 ###
 
-import logging 
+import logging
+from datetime import datetime
 
 from google.appengine.ext import db
 from google.appengine.ext.webapp import template
@@ -26,6 +27,9 @@ from django import newforms as forms
 from django.newforms.util import ErrorList, ValidationError
 
 import utils
+from utils import dbhooks
+
+from vendor.markdown import markdown
 
 STATUS_CHOICES = (
 	("draft",     "Draft"),
@@ -34,32 +38,71 @@ STATUS_CHOICES = (
 )
 
 
-class Entry(db.Model):
+class Entry(dbhooks.HookModel):
 	author = db.UserProperty()
 	title = db.StringProperty(required=True)
 	status = db.StringProperty(required=True, default=STATUS_CHOICES[0][0], choices=[x[0] for x in STATUS_CHOICES])
 	body = db.TextProperty(required=True)
 	tags = db.ListProperty(db.Key)
-	pubdate = db.DateProperty()
-	pubtime = db.TimeProperty()
+	pubdate = db.DateTimeProperty()
 	created = db.DateTimeProperty(auto_now_add=True)
 	updated = db.DateTimeProperty(auto_now=True)
-	
+		
 	# Next items are dynamically generated
 	cache_slug = db.UnindexedProperty()
-	slug = utils.MagicProperty(title, len, cache_prop=cache_slug, required=True)
-	cache_html= db.UnindexedProperty()
-	html = utils.MagicProperty(body, len, cache_prop=cache_html, required=True)
+	@utils.MagicProperty(title, cache_prop=cache_slug, pass_instance=True, required=True)
+	def slug(instance, s):
+		s = utils.slugify(s)
+		
+		ss = s
+		ss = ss.decode('utf-8')
+		def _unique():
+			q = Entry.all().filter("slug =", ss)
+			e = q.get()
+			
+			if e is None:
+				return False
+			
+			if instance.is_saved():
+				if instance.key() == e.key():
+					return False
+			
+			return True
+		
+		counter = 0
+		while _unique():
+			ss = "%s-%s" % (s, counter)
+			counter += 1
+		
+		return ss
 	
-	def delete(self, *args, **kw):
-		super(Entry, self).delete(*args, **kw)
-		count = utils.Count()
-		count.clear("entry", self.uripath)
+	cache_html = db.UnindexedProperty()
+	@utils.MagicProperty(body, cache_prop=cache_html, required=True)
+	def html(s):
+		return markdown.markdown(s)
 	
 	@property
 	def hits(self):
 		count = utils.Count()
-		return count.curcount("entry", self.slug)
+		return count.curcount("entry", str(self.key()))
+	
+	def delete(self, *args, **kw):
+		count = utils.Count()
+		count.clear("entry", str(self.key()))
+		super(Entry, self).delete(*args, **kw)
+	
+	def before_put(self):
+		# Because there is no way to specify that a property has a dependency on another we fake a get, so that the cache is primed
+		prime_cache = self.slug
+		prime_cache = self.html
+		
+		# Check to see if the status has changed to published, or from published to non-published.
+		
+		if self.status == STATUS_CHOICES[2][0]:
+			if self.pubdate is None:
+				self.pubdate = datetime.utcnow()
+		else:
+			self.pubdate = None
 
 class EntryForm(djangoforms.ModelForm):
 	status  = forms.CharField(widget=forms.Select(choices=STATUS_CHOICES), required=True, label='Status')
